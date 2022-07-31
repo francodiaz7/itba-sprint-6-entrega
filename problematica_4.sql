6b6361bd12db5c518923c3573081972224e302aa
Facundo Llanos <76456958+facundollanos@users.noreply.github.com>
2022-07-31 17:00:56 -0300

    item 2 y 4

--- problematica_4.sql
+++ problematica_4.sql
@@ -1,4 +1,26 @@
+#item1
 select  b.branch_name, count(b.branch_name)
 from cliente a inner join sucursal b
 on b.branch_id = a.branch_id
 GROUP by b.branch_name
+
+#item2
+select  b.branch_name, count(b.branch_name), count(employee_id)
+from cliente a inner join sucursal b
+on b.branch_id = a.branch_id
+
+inner join empleado c on c.branch_id = a.branch_id
+
+GROUP by b.branch_name
+
+
+#item4
+select  a.branch_name, avg(c.loan_total)
+from cliente b inner join prestamo c
+on c.customer_id = b.customer_id
+
+inner join sucursal a
+on b.branch_id = a.branch_id
+group by a.branch_name
+
+
